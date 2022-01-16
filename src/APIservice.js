0ec4970b65d65e721c754ca25da3e85a72b4a165
galinagumenyuk <gymenyuk.galina@gmail.com>
2022-01-16 23:38:10 +0200

    movies by key word

--- src/APIservice.js
+++ src/APIservice.js
@@ -1,7 +1,16 @@
 const KEY = "fa7d3ec0cb79e3ec102f854156943f99";
 const BASE_URL = "https://api.themoviedb.org/3";
+
 export function fetchPopularMovies() {
-  return fetch(`${BASE_URL}/trending/movie/week?api_key=${KEY}`)
+  return fetch(`${BASE_URL}/trending/all/day?api_key=${KEY}`)
+    .then((res) => res.json())
+    .then((data) => data.results);
+}
+
+export function fetchMovieByKeyWord(query) {
+  return fetch(
+    `${BASE_URL}/search/movie?api_key=${KEY}&query=${query}&language=en-US&page=1&include_adult=false`
+  )
     .then((res) => res.json())
     .then((data) => data.results);
 }
--- src/Movies/Movies.jsx
+++ src/Movies/Movies.jsx
@@ -1,4 +1,47 @@
-const Movies = () => { 
-   return( <p>Movies</p>)
-}
+import { useState, useEffect } from "react";
+import * as APIservice from "../APIservice";
+import { Link } from "react-router-dom";
+import s from "./Movies.module.css";
+
+const Movies = () => {
+    const [inputText, setInputText] = useState("");
+    const [query, setQuery] = useState("");
+    const [movies, setMovies] = useState("");
+
+    const handleNameChange = (e) => {
+        setInputText(e.currentTarget.value.toLowerCase())
+    };  
+    
+    const handleClick = (e) => {
+        e.preventDefault();
+         setQuery(inputText); 
+    }
+    
+
+    useEffect(() => {
+        if (query.trim() === "") {
+            return;
+        }
+            APIservice.fetchMovieByKeyWord(query)
+                .then(setMovies)
+        }, [query]);
+
+        return (
+            <div>
+                <input
+                    type="text"
+                    autoComplete="off"
+                    autoFocus
+                    onChange={handleNameChange}
+                ></input>
+                <button type='submit' onClick={handleClick}>Search</button>
+                <ul>
+                {movies && movies.map(movie => <li key={movie.id} className={ s.item}>
+                <Link to={`/movies/${movie.id}`} className={s.link }> {movie.title}</Link>
+                </li>) }
+                </ul>
+            </div>
+        )
+    }
+
 export default Movies;
